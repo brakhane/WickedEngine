a8a932d73e47d0bb36005678039c54f53acccbac
Dennis Brakhane <brakhane@gmail.com>
2023-10-23 16:54:50 +0200

    Jolt submodule

--- Editor/startup.lua
+++ Editor/startup.lua
@@ -2,6 +2,6 @@ SetProfilerEnabled(false)
 physics.SetAccuracy(1)
 
 -- You can set custom scaling for 2D screen elements here (GUI, sprites, fonts):
---local canvas = application.GetCanvas()
---canvas.SetCustomScaling(1.5)
---application.SetCanvas(canvas)
+local canvas = application.GetCanvas()
+canvas.SetCustomScaling(1.5)
+application.SetCanvas(canvas)
